b52780ab32e8939fa50d27618e43de7e28017cc1
Lucas Anjos <lucasoneplanet@gmail.com>
2023-01-12 08:20:28 -0300

    offline mode with shared preferences

--- lib/app/modules/home/external/dio/dio_get_pictures_of_the_day.dart
+++ lib/app/modules/home/external/dio/dio_get_pictures_of_the_day.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:nasa_app/app/constants/http_constants.dart';
 import 'package:nasa_app/app/modules/home/domain/errors/errors.dart';
@@ -5,6 +7,7 @@ import 'package:nasa_app/app/modules/home/domain/usecases/get_pictures_of_the_da
 import 'package:nasa_app/app/modules/home/domain/entities/nasa_apod.dart';
 import 'package:nasa_app/app/modules/home/external/mapper/mapper.dart';
 import 'package:nasa_app/app/modules/home/infra/datasources/get_pictures_of_the%20day_datasource.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DioGetPicturesOfTheDay implements GetPicturesOfTheDayDatasource {
   final Dio dio = Dio();
@@ -15,12 +18,21 @@ class DioGetPicturesOfTheDay implements GetPicturesOfTheDayDatasource {
       final response = await dio
           .get('https://api.nasa.gov/planetary/apod?${params.toUrlParams()}');
       if (response.statusCode == HttpConstants.REQUEST_SUCCESS) {
-        return MapperGetPicturesOfTheDay.toListNasaApod(response.data);
+        final data = MapperGetPicturesOfTheDay.toListNasaApod(response.data);
+        await _saveInCache(data);
+        return data;
       } else {
         throw GetPicturesOfTheDayException('API Error ${response.statusCode}');
       }
     } catch (e) {
+      print(e.toString());
       throw GetPicturesOfTheDayException('Internal Error');
     }
+  }
+
+  _saveInCache(List<NasaApod> data) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('pictures_data', jsonEncode(data));
+    print(prefs.get('pictures_data'));
   }
 }
